961b2a3871ade35814f638d7b123a8a25d9b9467
Bidfca <bruninhakjurugat11@gmail.com>
2020-11-17 13:18:41 -0300

    form done

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -16,8 +16,7 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-let ul = document.createElement("ul")
-ul.className = "days";
+let ul = document.getElementById("days")
 
 dezDaysList.forEach(day => {
   let li = document.createElement("li");
@@ -33,3 +32,62 @@ dezDaysList.forEach(day => {
 
   ul.appendChild(li);
 })
+
+function criaBotao(feriados) {
+  let botao = document.createElement("button");
+  botao.setAttribute("id", "btn-holiday");
+  botao.innerText = feriados;
+  let div = document.querySelector(".buttons-container");
+  div.appendChild(botao);
+}
+
+criaBotao("Feriados");
+
+let botao = document.getElementById("btn-holiday");
+botao.addEventListener("click", alternarCor);
+
+function alternarCor() {
+  let holidays = document.getElementsByClassName("holiday");
+  for (const day of holidays) {
+    const color = day.style.backgroundColor;
+    day.style.backgroundColor = color === "white" ? "rgb(238,238,238)" : "white"
+  }
+}
+
+
+function criaBotao2(sextafeira) {
+  let botao = document.createElement("button");
+  botao.setAttribute("id", "btn-friday");
+  botao.innerText = sextafeira;
+  let div = document.querySelector(".buttons-container");
+  div.appendChild(botao);
+}
+
+criaBotao2("Sexta-feira");
+
+let botao2 = document.getElementById("btn-friday");
+botao2.addEventListener("click", alternarTexto);
+const fridays_numbers = [4,11,18,25];
+function alternarTexto() {
+  let fridays = document.getElementsByClassName("friday");
+  for (let index = 0; index < fridays.length; index++) {
+    fridays[index].innerText = fridays[index].innerText === "SEXTOU" ? fridays_numbers[index] : "SEXTOU";
+  }
+}
+
+let lis = document.getElementsByClassName("day");
+
+for(const day of lis) {
+  day.addEventListener("mouseover", zoomIn);
+  day.addEventListener("mouseout", zoomOut);
+}
+
+function zoomIn(event) {
+  event.target.style.fontSize = "25px";
+}
+
+function zoomOut(event) {
+  event.target.style.fontSize = "20px";
+}
+
+
