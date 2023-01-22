38e5da370a93f527128b08210ebd55dd7162f9ca
Bianca Caetano <60355496+Bidfca@users.noreply.github.com>
2023-01-22 00:46:37 -0300

    Delete rename.py

--- rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = os.path.dirname(os.path.abspath(__file__))
-files = os.listdir(path)
-
-for index, file in enumerate(files):
-  if '.png' not in file:
-    os.rename(os.path.join(path, file), os.path.join(path, file + ".png"))
